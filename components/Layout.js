ada1f441435811d188dfa62f191fdddac529b9d9
Rhoda P <rhodaphillipsosei@gmail.com>
2022-05-10 09:53:02 -0400

    import nav details from old project file

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav.js";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="content">
+      <Nav />
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/Nav.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Nav } from "react-bootstrap/Nav";
+import { AiFillGithub, AiFillLinkedin, AiFillHome, AiTwotoneMail } from "react-icons/Ai";
+import Link from "next/link";
+
+
+export default Nav = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+     
+        <div className="container-fluid" id="icon-container">
+          <div>
+           <Link href="/">
+          <a id="homelink">
+            <AiFillHome />
+          </a>
+        </Link>
+        
+            <div id="social-icons">
+              <Link href="https://linkedin.com/in/rphillipsosei">
+                <a id="navlink">
+                  <AiFillLinkedin />
+                </a>
+              </Link>
+              <Link href="https://github.com/rphillipsosei">
+                <a id="navlink">
+                  <AiFillGithub />
+                </a>
+              </Link>
+              <Link href="/contact">
+                <a id="navlink">
+                  <AiTwotoneMail />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
