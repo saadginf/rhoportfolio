a087d55a56089a4798efee8ff1f8142859c29d3e
Rhoda P <rhodaphillipsosei@gmail.com>
2022-05-13 22:18:08 -0400

    final positioning nav icons, resizing colour cards

--- components/Nav.js
+++ components/Nav.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { AiFillGithub, AiFillLinkedin, AiFillHome } from "react-icons/ai";
-import styles from "../styles/Home.module.css";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Link from "next/link";
-
+import styles from "../styles/Home.module.css"
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid py-3" id="icon-container">
-          <div className={styles.getintouch}>GET IN TOUCH</div>
-          <div id="social-icons">
-            <Link href="https://linkedin.com/in/rphillipsosei">
-              <a id="nav-link">
-                <AiFillLinkedin />
-              </a>
-            </Link>
-            <Link href="https://github.com/rphillipsosei">
-              <a id="nav-link">
-                <AiFillGithub />
-              </a>
-            </Link>
+        <div className="container-fluid py-3" id="nav-contents">
+          <div>GET IN TOUCH</div>
+          <div>
+            <div className={styles.icons}>
+              <Link href="https://linkedin.com/in/rphillipsosei">
+                <a id="nav-link">
+                  <AiFillLinkedin />
+                </a>
+              </Link>
+              <Link href="https://github.com/rphillipsosei">
+                <a id="nav-link">
+                  <AiFillGithub />
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
